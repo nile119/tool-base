255fe014b67936bf55094c5e8e319e5ca31cf91e
nile119 <belousov_alexander@rambler.ru>
2020-05-25 23:25:51 +0500

    Add files via upload

--- tool.py
+++ tool.py
@@ -6,16 +6,37 @@ except: import Tkinter as tk
 # --- Заглавное окно ---
 master = tk.Tk()
 master.title('База импортного инструмента')
+master.minsize(390,250)
+master.maxsize(390,250)
 
 okno1 = tk.LabelFrame(master)
-okno1.grid()
+okno1.pack(padx = 10,
+           pady = 10,
+           ipadx = 50)
 
-# --- Кнопка ввода нового инструмента ---
+# --- Кнопка перехода в окно учёта поступления инструмента ---
 knopka_vvod = tk.Button(okno1,
-                        text = 'Ввести новый инструмент')
-knopka_vvod.grid(row  = 1,
-                 padx = 50,
-                 pady = 50)
+                        width = 20,
+                        height = 2,
+                        text = 'Ввести инструмент')
+knopka_vvod.pack(padx = 30,
+                 pady = 20)
+
+# --- Кнопка перехода в окно учёта выдачи инструмента ---
+knopka_vydacha = tk.Button(okno1,
+                           width = 20,
+                           height = 2,
+                           text = 'Выдать инструмент')
+knopka_vydacha.pack(padx = 30,
+                    pady = 0)
+
+# --- Кнопка перехода к информации о программе ---
+knopka_info = tk.Button(okno1,
+                        width = 20,
+                        height = 2,
+                        text = 'О программе...')
+knopka_info.pack(padx = 30,
+                 pady = 20)
 
 okno1.mainloop()
 
