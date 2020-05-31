94f38a114cad3ddba53434bc0585f00d04fc1bd5
nile119 <belousov_alexander@rambler.ru>
2020-05-31 18:33:08 +0500

    Add files via upload

--- tool.py
+++ tool.py
@@ -6,19 +6,22 @@ except: import Tkinter as tk
 
 master = tk.Tk()
 master.title('База импортного инструмента')
-master.minsize(390,340)
-master.maxsize(390,340)
+#master.minsize(640,420)
+#master.maxsize(640,420)
 
-# --- Заглавное окно ---
+def okno(a):
+    a.pack(fill='both',
+           padx = 20,
+           pady = 20,
+           ipadx = 50,
+           expand=1)
+           
+####################################
+###   Заглавное окно ("Окно 0")  ###
+####################################
+
 okno0 = tk.LabelFrame(master)
-okno0.pack(padx = 10,
-           pady = 10,
-           ipadx = 50)
-
-# --- Окно справки --- 
-okno3 = tk.LabelFrame(master)
-
-###               Заглавное окно               ###
+okno(okno0)
 
 # --- Кнопка перехода в окно учёта поступления инструмента ---
 knopka_vvod = tk.Button(okno0,
@@ -39,10 +42,7 @@ knopka_vydacha.pack(padx = 30,
 # --- Кнопка перехода к информации о программе ---
 def infoo():
     okno0.pack_forget()
-   
-    okno3.pack(padx = 10,
-               pady = 10,
-               ipadx = 50)
+    okno(okno3)
 
 knopka_info = tk.Button(okno0,
                         width = 20,
@@ -61,30 +61,40 @@ knopka_vihod = tk.Button(okno0,
                          height = 2,
                          text = 'Выход',
                          command=vyhod)
-knopka_vihod.pack(padx = 30,
+knopka_vihod.pack(side='bottom',
+                  padx = 30,
                   pady = 20)
 
-###                  Окно справки                  ###    
-tk.Label(okno3,text='- - -').pack(pady=15,anchor='center')
+###################################
+###   Окно справки ("Окно 3")   ###
+###################################
+
+okno3 = tk.LabelFrame(master)
+
+# --- Справочный текст ---
+
+tekst3=tk.Frame(okno3)
+tekst3.pack(pady=25)
+
 for i in open('info.txt'):
-    tk.Label(okno3,
-             text=i.rstrip()).pack(anchor='sw',
-                                   padx=15)
-tk.Label(okno3,text='- - -').pack(pady=15,anchor='center')
+    tk.Label(tekst3,
+             text=i.rstrip()).pack(anchor='w',
+                                   padx=5)
 
 # --- Кнопка выхода ---
+
 def nazad():
     okno3.pack_forget()
-    okno0.pack(padx = 10,
-               pady = 10,
-               ipadx = 30)
+    okno(okno0)
 
 knopka_nazad = tk.Button(okno3,
-                         width = 20,
+                         width = 10,
                          height = 2,
                          text = 'Назад',
                          command=nazad)
-knopka_nazad.pack(padx = 30,
-                  pady = 20)
+knopka_nazad.pack(side='bottom',
+                  anchor='w',
+                  padx=30,
+                  pady=20)
 
 tk.mainloop()
