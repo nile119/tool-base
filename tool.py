318aa4e812ee17d1cd2175ae5e27b408fd952751
nile119 <belousov_alexander@rambler.ru>
2020-06-07 20:20:09 +0500

    Add files via upload

--- tool.py
+++ tool.py
@@ -6,15 +6,47 @@ except: import Tkinter as tk
 
 master = tk.Tk()
 master.title('База импортного инструмента')
-#master.minsize(640,420)
+master.minsize(600,20)
 #master.maxsize(640,420)
 
-def okno(a):
+def okno(a):             # Отобразить окно
     a.pack(fill='both',
            padx = 20,
            pady = 20,
            ipadx = 40,
            expand=1)
+
+def back_b(a,b):         # Кнопка "Назад"
+    def nazad():
+        a.pack_forget()
+        okno(b)
+    knopka_nazad = tk.Button(a,
+                             width = 10,
+                             height = 2,
+                             text = 'Назад',
+                             command=nazad)
+    knopka_nazad.pack(side='left',
+                      anchor='w',
+                      padx=30,
+                      pady=20,
+                      expand=1)
+
+def forw_b(a,b):         # Кнопка "Далее"
+    def nazad():
+        a.pack_forget()
+        okno(b)
+    knopka_nazad = tk.Button(a,
+                             width = 10,
+                             height = 2,
+                             text = 'Далее',
+                             command=nazad)
+    knopka_nazad.pack(side='right',
+                      anchor='e',
+                      padx=30,
+                      pady=20,
+                      expand=1)
+
+
            
 ####################################
 ###   Заглавное окно ("Окно 0")  ###
@@ -26,7 +58,7 @@ okno(okno0)
 # --- Кнопка перехода в окно учёта поступления инструмента ---
 def vvod_tool():
     okno0.pack_forget()
-    okno(okno1)
+    okno(okno1_1)
 
 knopka_vvod = tk.Button(okno0,
                         width = 20,
@@ -74,45 +106,84 @@ knopka_vihod.pack(side='bottom',
 ###   Окно ввода инструмента ("Окно 1")  ###
 ############################################
 
-okno1 = tk.LabelFrame(master)
+### Окно 1_1 Шаг 1: Выберите тип инструмента ###
 
-forma1=tk.Frame(okno1)
-forma1.pack(pady=15)
+okno1_1 = tk.LabelFrame(master)
 
+forma1_1=tk.Frame(okno1_1)
+forma1_1.pack(pady=15)
+
+shag_1 = tk.Label(forma1_1,
+                  text = 'Шаг 1: Выберите тип инструмента',
+                  anchor = 'w').pack(pady = 20)
 # --- Ввод типа инструмента ---
 
-shag_1 = tk.Label(forma1,
-                  #width = 25,
-                  text = 'Шаг 1: Выберите тип инструмента',
-                  anchor = 'w').grid(row = 1,
-                                     column = 1,
-                                     columnspan=2,
-                                     pady = 20)
+tip_text = tk.Label(forma1_1,
+                    width=25,
+                    text = 'Тип:')
 
-tip_text = tk.Label(forma1,
-                   width=25,
-                   text = 'Тип:')
-                  # anchor='e')
-tip_text.grid(row = 2,
-              column = 1,
-              pady = 2,sticky='e')
+tip_text.pack(pady = 2)
+              
 
 var1=tk.StringVar(master)
-var1.set('Вращающийся инструмент')
+var1.set('')#'Вращающийся инструмент')
 
-tip_vvod = tk.OptionMenu(forma1,
+tip_vvod = tk.OptionMenu(forma1_1,
                          var1,
-                         'Вращающийся инструмент',
-                         'Токарный инструмент',
-                         'Пластины',
+                         'Вращающийся инструмент    ',
+                         'Токарный инструмент       ',
+                         'Пластины                  ',
                          'Вспомогательный инструмент',
-                         'Другое')
+                         'Другое                    ')
+tip_vvod.pack(pady=2)
 
-tip_vvod.grid(row = 2,
-              column = 2,
-              pady=2,sticky='w')
+# --- Кнопка выхода ---
+back_b(okno1_1,okno0)
 
-shag_2 = tk.Label(forma1,
+
+
+### Окно 1_2 Шаг 2: Введите данные инструмента ###
+
+okno1_2 = tk.LabelFrame(master)
+
+# --- Кнопка далее ---
+
+def forw():         # Кнопка "Далее"
+    okno1_1.pack_forget()
+    okno(okno1_2)
+def switchButtonState(*args):
+    if (var1.get()==''):
+        knopka_vpered['state'] = tk.DISABLED
+    else:
+        knopka_vpered['state'] = tk.NORMAL
+knopka_vpered = tk.Button(okno1_1,
+                          state=tk.DISABLED,
+                          width = 10,
+                          height = 2,
+                          text = 'Далее',
+                          command=forw)
+knopka_vpered.pack(side='right',
+                   anchor='e',
+                   padx=30,
+                   pady=20,
+                   expand=1)
+
+var1.trace("w",switchButtonState)
+
+#if var1.get()=='':
+ #   knopka_vpered['state'] = tk.DISABLED
+#        s='disabled'
+#else:
+ #   knopka_vpered['state'] = tk.NORMAL
+
+
+
+# ---
+
+forma1_2=tk.Frame(okno1_2)
+forma1_2.pack(pady=15)
+
+shag_2 = tk.Label(forma1_2,
                   text = 'Шаг 2: Введите данные инструмента',
                   anchor = 'w').grid(row = 3,
                                      column = 1,
@@ -120,43 +191,33 @@ shag_2 = tk.Label(forma1,
                                      pady = 20)
 
 # --- Наименование инструмента ---
-name_text = tk.Label(forma1,
+
+name_text = tk.Label(forma1_2,
                      width = 25,
                      text = 'Наименование:',
                      anchor = 'w').grid(row = 4,
                                         column = 1,
                                         pady = 2)
-name_vvod = tk.Entry(forma1,
+name_vvod = tk.Entry(forma1_2,
                      width = 35).grid(row = 4,
                                       column = 2)
 
 # --- Производитель/Стандарт ---
-brand_text = tk.Label(forma1,
+brand_text = tk.Label(forma1_2,
                      width = 25,
                      text = 'Производитель (или ГОСТ):',
                      anchor = 'w').grid(row = 5,
                                         column = 1,
                                         pady = 2)
-brand_vvod = tk.Entry(forma1,
+brand_vvod = tk.Entry(forma1_2,
                      width = 35).grid(row = 5,
                                       column = 2)
-
 # --- Кнопка выхода ---
-def back_b(a):
-    def nazad():
-        a.pack_forget()
-        okno(okno0)
+back_b(okno1_2,okno1_1)
 
-    knopka_nazad = tk.Button(a,
-                             width = 10,
-                             height = 2,
-                             text = 'Назад',
-                             command=nazad)
-    knopka_nazad.pack(side='bottom',
-                      anchor='w',
-                      padx=30,
-                      pady=20)
-back_b(okno1)
+# --- Кнопка далее ---
+forw_b(okno1_2,okno1_2)
+
 
 ###################################
 ###   Окно справки ("Окно 3")   ###
@@ -175,20 +236,7 @@ for i in open('info.txt'):
                                    padx=5)
 
 # --- Кнопка выхода ---
-def back_b(a):
-    def nazad():
-        a.pack_forget()
-        okno(okno0)
-
-    knopka_nazad = tk.Button(a,
-                             width = 10,
-                             height = 2,
-                             text = 'Назад',
-                             command=nazad)
-    knopka_nazad.pack(side='bottom',
-                      anchor='w',
-                      padx=30,
-                      pady=20)
-back_b(okno3)
+back_b(okno3,okno0)
 
 tk.mainloop()
+
