e1272453627160aca3f7f62deab91bb56e680a93
nile119 <belousov_alexander@rambler.ru>
2020-06-08 00:14:44 +0500

    Add files via upload

--- tool.py
+++ tool.py
@@ -6,7 +6,7 @@ except: import Tkinter as tk
 
 master = tk.Tk()
 master.title('База импортного инструмента')
-master.minsize(600,20)
+master.minsize(640,20)
 #master.maxsize(640,420)
 
 def okno(a):             # Отобразить окно
@@ -126,15 +126,15 @@ tip_text.pack(pady = 2)
               
 
 var1=tk.StringVar(master)
-var1.set('')#'Вращающийся инструмент')
+var1.set('')
 
 tip_vvod = tk.OptionMenu(forma1_1,
                          var1,
-                         'Вращающийся инструмент    ',
-                         'Токарный инструмент       ',
-                         'Пластины                  ',
+                         'Вращающийся инструмент',
+                         'Токарный инструмент',
+                         'Пластины',
                          'Вспомогательный инструмент',
-                         'Другое                    ')
+                         'Другое')
 tip_vvod.pack(pady=2)
 
 # --- Кнопка выхода ---
@@ -149,6 +149,13 @@ okno1_2 = tk.LabelFrame(master)
 # --- Кнопка далее ---
 
 def forw():         # Кнопка "Далее"
+
+    if var1.get()==u'Вращающийся инструмент':
+        diam_text.grid(row=6,column=1,pady=2)
+        diam_vvod.grid(row=6,column=2,pady=2)
+    elif var1.get()==u'Пластины':
+        material_text.grid(row=4,column=1,pady=2)
+        material_vvod.grid(row=4,column=2,pady=2)
     okno1_1.pack_forget()
     okno(okno1_2)
 def switchButtonState(*args):
@@ -170,14 +177,6 @@ knopka_vpered.pack(side='right',
 
 var1.trace("w",switchButtonState)
 
-#if var1.get()=='':
- #   knopka_vpered['state'] = tk.DISABLED
-#        s='disabled'
-#else:
- #   knopka_vpered['state'] = tk.NORMAL
-
-
-
 # ---
 
 forma1_2=tk.Frame(okno1_2)
@@ -185,35 +184,74 @@ forma1_2.pack(pady=15)
 
 shag_2 = tk.Label(forma1_2,
                   text = 'Шаг 2: Введите данные инструмента',
-                  anchor = 'w').grid(row = 3,
+                  anchor = 'w').grid(row = 1,
                                      column = 1,
                                      columnspan=2,
                                      pady = 20)
 
 # --- Наименование инструмента ---
+name_text = tk.Label(forma1_2, width = 35, text = 'Наименование:',anchor = 'w')
+name_vvod = tk.Entry(forma1_2, width = 35)
 
-name_text = tk.Label(forma1_2,
-                     width = 25,
-                     text = 'Наименование:',
-                     anchor = 'w').grid(row = 4,
-                                        column = 1,
-                                        pady = 2)
-name_vvod = tk.Entry(forma1_2,
-                     width = 35).grid(row = 4,
-                                      column = 2)
+# --- Номер инструмента ---
+nomer_text = tk.Label(forma1_2, width = 35, text = 'Номер по каталогу / ГОСТу / заводской:',anchor = 'w')
+nomer_vvod = tk.Entry(forma1_2, width = 35)
+
+# --- Материал ---
+material_text = tk.Label(forma1_2, width = 35, text = 'Материал:',anchor = 'w')
+material_vvod = tk.Entry(forma1_2, width = 35)
 
 # --- Производитель/Стандарт ---
-brand_text = tk.Label(forma1_2,
-                     width = 25,
-                     text = 'Производитель (или ГОСТ):',
-                     anchor = 'w').grid(row = 5,
-                                        column = 1,
-                                        pady = 2)
-brand_vvod = tk.Entry(forma1_2,
-                     width = 35).grid(row = 5,
-                                      column = 2)
+brand_text = tk.Label(forma1_2, width = 35, text = 'Производитель (или ГОСТ):', anchor = 'w')
+brand_vvod = tk.Entry(forma1_2, width = 35)
+
+# --- Диаметр ---
+diam_text = tk.Label(forma1_2, width = 35, text = 'Радиус, мм:', anchor = 'w')
+diam_vvod = tk.Entry(forma1_2, width = 35)
+
+# --- Количество ---
+kolichestvo_text = tk.Label(forma1_2, width = 35, text = 'Количество, шт:', anchor = 'w')
+kolichestvo_vvod = tk.Entry(forma1_2, width = 35)
+
+# --- Комментарий ---
+comment_text = tk.Label(forma1_2, width = 35, text = 'Комментарий:', anchor = 'w')
+comment_vvod = tk.Entry(forma1_2, width = 35)
+
+name_text.grid(row=2,column=1,pady=2)
+name_vvod.grid(row=2,column=2,pady=2)
+nomer_text.grid(row=3,column=1,pady=2)
+nomer_vvod.grid(row=3,column=2,pady=2)
+brand_text.grid(row=5,column=1,pady=2)
+brand_vvod.grid(row=5,column=2,pady=2)
+kolichestvo_text.grid(row=7,column=1,pady=2)
+kolichestvo_vvod.grid(row=7,column=2,pady=2)
+comment_text.grid(row=8,column=1,pady=2)
+comment_vvod.grid(row=8,column=2,pady=2)
+
+
+
 # --- Кнопка выхода ---
-back_b(okno1_2,okno1_1)
+
+
+def nazad_1_2():
+
+    diam_text.grid_remove()
+    diam_vvod.grid_remove()
+    material_text.grid_remove()
+    material_vvod.grid_remove()
+    okno1_2.pack_forget()
+    okno(okno1_1)
+knopka_nazad_1_2 = tk.Button(okno1_2,
+                             width = 10,
+                             height = 2,
+                             text = 'Назад',
+                             command=nazad_1_2)
+knopka_nazad_1_2.pack(side='left',
+                      anchor='w',
+                      padx=30,
+                      pady=20,
+                      expand=1)
+
 
 # --- Кнопка далее ---
 forw_b(okno1_2,okno1_2)
